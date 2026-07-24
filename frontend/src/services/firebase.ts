525ab814037abee3fa16f2bcb21264c94c969e8c
Hemant Bhujang <mrhemant619@gmail.com>
2026-07-24 19:52:59 +0530

    Change in Firebase

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD66rxt-HhK7KIiduhSP5AVWaALpFSv5_w",
+  apiKey: "Google API Key",
   authDomain: "budgetminds-7cbbc.firebaseapp.com",
   projectId: "budgetminds-7cbbc",
   storageBucket: "budgetminds-7cbbc.firebasestorage.app",
